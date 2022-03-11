febe9927b2489f1ee78c2b3c1ed2aac5c576a85e
Devyani-ein <99027867+Devyani-ein@users.noreply.github.com>
2022-03-11 21:27:49 +0530

    Add files via upload

---
+++ Q5.c
@@ -0,0 +1,32 @@
+//Q5 Devyani Mahale
+
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+char buff[]="\0";
+int main()
+{
+     int fd;
+     if((fd = creat("file1.txt", 777)) < 0)
+     {
+            perror("Creation error");
+	    exit(1);
+     }
+     
+   
+     if(lseek(fd,4094,SEEK_SET) < 0)
+     {
+	    perror("Positioning error");
+            exit(2);
+     }
+     if(write(fd,buff,sizeof(buff)) < 0)
+     {
+	    perror("Writing error");
+            exit(3);
+     }
+return 0;
+
+}
+    		   

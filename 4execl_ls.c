9a635db45fa7e091fe610894074149b89497dd43
Devyani-ein <99027867+Devyani-ein@users.noreply.github.com>
2022-03-17 11:35:14 +0530

    Add files via upload

---
+++ 4execl_ls.c
@@ -0,0 +1,14 @@
+//Question 4
+
+#include<stdio.h>
+#include<unistd.h>
+
+int main()
+{
+	printf("This is the program for replacing new command line program\n");
+
+	execl("/bin/ls", "ls", "-lh", "-a" , 0);
+
+
+	return 0;
+}

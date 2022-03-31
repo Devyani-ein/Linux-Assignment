739b72acbc8d9be281774722305cf539f5b4d792
Devyani-ein <99027867+Devyani-ein@users.noreply.github.com>
2022-03-31 23:06:15 +0530

    Add files via upload

---
+++ Que4_priority.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+ * Devyani Mahale
+ *Question-4 Write a program which get and set pthread scheduling policy an d priority
+*******************************************************************************/
+#include<pthread.h>
+#include<stdio.h>
+
+void * my(void *i)
+{
+printf("\n i am in %d thread\n",(int *)i);
+}
+int main()
+{
+pthread_t tid;
+struct sched_param param;
+int priority,policy,ret;
+
+ret =pthread_getschedparam(pthread_self(), &policy,&param);
+
+printf("\n-----------------main thread---------\n policy : %d \tPriority%d\n",policy,param.sched_priority);
+
+policy=SCHED_FIFO;
+param.sched_priority =3;
+
+pthread_setschedparam(pthread_self(),policy,&param);
+pthread_getschedparam(pthread_self(),&policy,&param);
+printf("\n-------------------main thread----------------\n policy; %d\tpriroty:%d\n",policy,param.sched_priority)
+return 0;
+}
+
